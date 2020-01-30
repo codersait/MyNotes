ee693561a749cf80c1c58d11a98a03ac4f7484f4
codersait <codersait@gmail.com>
2020-01-30 10:51:07 +0300

    my first notes

---
+++ Simplifying_If_Statements.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // First way to implement the scenario
+
+        int income = 120_000;
+        boolean hasHighIncome;
+        if (income > 100_000)
+            hasHighIncome = true;
+        else
+            hasHighIncome = false;
+
+        // Second Way
+
+        int income = 120_000;
+        boolean hasHighIncome = false;
+        if (income > 100_000)
+            hasHighIncome = true;
+
+        // Simplest, elegant and most professional way
+        int income = 120_000;
+        boolean hasHighIncome = (income > 100_000);
+
+
+    }
+
+}
